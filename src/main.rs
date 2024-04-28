eb021171c8b8db28d63f6effe8f8b2e9d1aa56ce
Satake Makoto <satake.syukatsu@gmail.com>
2024-04-28 21:23:54 +0900

    Parse args using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct AppArg {
+    #[clap(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Fue { task: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = AppArg::parse();
+    if let Some(command) = cli.command {
+        match command {
+            Command::Fue { task } => {
+                // Add Task
+
+                // Print
+                println!("Add {}", task)
+            }
+        }
+    } else {
+        // When no subcommand, list tasks
+        // List Tasks
+
+        // Print
+        println!("Task1, Task2, Task3...")
+    }
 }
